dcc7f598318cbbc748962a9a5f5c45a259cd7768
Wojciech Sebestowicz <w.sebestowic@cheil.com>
2020-11-18 23:18:17 +0100

    test

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database')
+
+mongoose.connect(config.database, { useUnifiedTopology: true , useNewUrlParser: true });
+
+mongoose.connection.on('DB connected', () => {
+    console.log('DB connected successfully ' + config.database)
+});
 
 const app = express();
 
@@ -12,6 +19,8 @@ const users = require('./routes/users');
 const port = 3000;
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(bodyParser.json());
 
